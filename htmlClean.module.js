c84359897b90e0da605a0e95b9afdb9f829100ce
darkenvy <darkenvy6@gmail.com>
2017-02-17 12:32:28 -0800

    fixes to distortions in irc

--- htmlClean.module.js
+++ htmlClean.module.js
@@ -5,29 +5,29 @@ module.exports = {
   get: function(html){
     // An object that alternates what it returns
     // probably can make shorter using a list[].
-    var alternateColor = new function() {
-      var current = 0;
-      var cycle = function() {
-        current++;
-        current = current % 2;
-        return current;
-      }
-      var get = function() {
-        if (cycle()) {return '\u000302';} // dark blue
-        else         {return '\u000301';} // black
-      }
-      return get;
-    }
+    // var alternateColor = new function() {
+    //   var current = 0;
+    //   var cycle = function() {
+    //     current++;
+    //     current = current % 2;
+    //     return current;
+    //   }
+    //   var get = function() {
+    //     if (cycle()) {return '\u000302';} // dark blue
+    //     else         {return '\u000301';} // black
+    //   }
+    //   return get;
+    // }
 
     html = html.slice(10, html.length-4); // cuts off <![CDATA[ ]]>
     html = html.replace(/<br>/g, ' ');
     var $ = cheerio.load(html);
 
     // Clean up Uglies
-    $('code').replaceWith( IRC.colors.wrap('light_gray', '[CodeBlock]') ) // colorize
-    $('blockquote').replaceWith( IRC.colors.wrap('light_gray', '[Quote]') ) // colorize
-    $('img').replaceWith( IRC.colors.wrap('light_gray', '[Image]') ) // colorize
-    $('a').replaceWith( IRC.colors.wrap('light_gray', '[Link]') ) // colorize
+    $('code').replaceWith( IRC.colors.wrap('gray', '[CodeBlock] ') ) // colorize
+    $('blockquote').replaceWith( IRC.colors.wrap('gray', '[Quote] ') ) // colorize
+    $('img').replaceWith( IRC.colors.wrap('gray', '[Image] ') ) // colorize
+    $('a').replaceWith( IRC.colors.wrap('gray', '[Link] ') ) // colorize
 
     // Beautify List
     var each = $('li')
@@ -39,7 +39,8 @@ module.exports = {
     // Final Cleanup. Rid newlines, replace with alternating colors
     // Fix multiple whitespaces
     var cleanedText = $.text();
-    cleanedText = cleanedText.replace(/(\n\s{0,})+/g, function(m){return alternateColor()}); 
+    // cleanedText = cleanedText.replace(/(\n\s{0,})+/g, function(m){return alternateColor()}); 
+    cleanedText = cleanedText.replace(/(\n\s{0,})+/g, ' '); 
     cleanedText = cleanedText.replace(/\s{2,}/g, ' ')
 
     return cleanedText;
